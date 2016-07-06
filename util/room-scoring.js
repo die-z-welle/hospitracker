5555e8756886a1d6947750f0b6ad37f0cb519963
aqulu <akku@outlook.com>
2016-07-06 13:43:56 +0200

    callbacks

--- util/room-scoring.js
+++ util/room-scoring.js
@@ -16,54 +16,44 @@ var transformRSSItoScore = function(rssi) {
 }
 
 var roomscore = function(beaconList, callback) {
+	Beacon.find({}).populate('room').exec(function(err, result) {
+  	var beacons = [];
+	  for(i in beaconList) {
+			var b = result.filter(function(value) {
+					return value.minor === beaconList[i].minor && value.major === beaconList[i].major && value.uuid === beaconList[i].uuid;
+				});
+			beacons.push(b);
+			beaconList[i].room = b.room;
+	  }
 
-  var beacons = [];
+		for (i in beacons) {
+			console.log(JSON.stringify(beacons[i]));
+		}
 
-  for(i in beaconList) {
-    //console.log(beaconList[i]);
-    populateBeacon(beaconList[i], function(beacon) {
-      if(beacon != null) {
-        beacons.push(beacon);
-      }
-    });
+	  var rooms = {};
+	  for(i in beaconList) {
+	    if(beaconList[i].hasOwnProperty("room")) {
+		    var rssi = beaconList[i].value;
+		    rooms[beaconList[i].room.identification] = transformRSSItoScore(rssi);
+	    }
+	  }
 
+	  var winner = null;
+	  var totalscore = 0;
+	  for(i in rooms) {
+	    totalscore += rooms[i];
+	    if(winner == null) {
+	      winner = i;
+	    } else {
+	      if(rooms[winner] < rooms[i]) {
+	        winner = i;
+	      }
+	    }
+	  }
 
-  }
-
-  for(i in beacons) {
-    console.log(beacons);
-  }
-
-  var rooms = {};
-
-  for(i in beaconList) {
-    if(beaconList[i].hasOwnProperty("room")) {
-      rooms[beaconList[i].room.identification] = 0;
-    }
-  }
-
-
-  for(i in beaconList) {
-    var rssi = beaconList[i].value;
-    rooms[beaconList[i].room.identification] += transformRSSItoScore(rssi);
-  }
-
-  var winner = null;
-  var totalscore = 0;
-  for(i in rooms) {
-    totalscore += rooms[i];
-    if(winner == null) {
-      winner = i;
-    } else {
-      if(rooms[winner] < rooms[i]) {
-        winner = i;
-      }
-    }
-  }
-
-  var accuracy = rooms[winner] / totalscore;
-
-  callback(winner, accuracy);
+	  var accuracy = rooms[winner] / totalscore;
+	  callback(winner, accuracy);
+	});
 }
 
 module.exports = roomscore;
